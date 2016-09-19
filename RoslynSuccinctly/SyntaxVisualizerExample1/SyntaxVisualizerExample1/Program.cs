af07d7b6196b3917a72bcca3cf00e2d92d400390
ValisStigma <rafael_krucker@hotmail.com>
2016-09-19 18:50:56 +0200

    Added example for Syntaxvisualizer

---
+++ RoslynSuccinctly/SyntaxVisualizerExample1/SyntaxVisualizerExample1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxVisualizerExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime oneDate = new DateTime(2015, 8, 19);
+
+            Console.WriteLine($"Today is {oneDate.ToShortDateString()}");
+            Console.ReadLine();
+        }
+    }
+}
