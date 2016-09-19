8b18240ba4e46d830c855f99c2f7370acff913d7
ValisStigma <rafael_krucker@hotmail.com>
2016-09-19 11:14:39 +0200

    first commit

---
+++ SemanticsCS/SemanticsCS/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SemanticsCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(
+            @"using System;
+            using System.Collections.Generic;
+            using System.Text;
+
+            namespace HelloWorld
+            {
+                class Program
+                {
+                    static void Main(string[] args)
+                    {
+                        Console.WriteLine(""Hello, World!"");
+                    }
+                }
+            }");
+
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+
+            var compilation = CSharpCompilation.Create("HelloWorld")
+                                               .AddReferences(
+                                                    MetadataReference.CreateFromFile(
+                                                        typeof(object).Assembly.Location))
+                                               .AddSyntaxTrees(tree);
+            var model = compilation.GetSemanticModel(tree);
+            var nameInfo = model.GetSymbolInfo(root.Usings[0].Name);
+            var systemSymbol = (INamespaceSymbol)nameInfo.Symbol;
+            foreach (var ns in systemSymbol.GetNamespaceMembers())
+            {
+                Console.WriteLine(ns.Name);
+            }
+            Console.Read();
+        }
+    }
+}
