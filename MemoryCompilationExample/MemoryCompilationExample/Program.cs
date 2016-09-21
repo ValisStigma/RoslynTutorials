d59698cd50109130d49fa29f5ca726a10011b4b2
ValisStigma <rafael_krucker@hotmail.com>
2016-09-21 09:18:51 +0200

    Added Example for In Memory Compilation

---
+++ MemoryCompilationExample/MemoryCompilationExample/Program.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Symbols;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.Text;
+
+namespace MemoryCompilationExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"
+            using System;
+
+            namespace RoslynCompileSample
+            {
+                public class Writer
+                {
+                    public void Write(string message)
+                    {
+                        Console.WriteLine(message);
+                        Console.Read();
+                    }
+                }
+            }");
+
+            var assemblyName = Path.GetRandomFileName();
+            var references = new MetadataReference[]
+            {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location)
+            };
+
+            var compilation = CSharpCompilation.Create(
+                assemblyName,
+                syntaxTrees: new [] {syntaxTree},
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+            );
+
+            using (var ms = new MemoryStream())
+            {
+                EmitResult result = compilation.Emit(ms);
+
+                if (!result.Success)
+                {
+                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                        diagnostic.IsWarningAsError ||
+                        diagnostic.Severity == DiagnosticSeverity.Error);
+
+                    foreach (Diagnostic diagnostic in failures)
+                    {
+                        Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                    }
+                }
+                else
+                {
+                    ms.Seek(0, SeekOrigin.Begin);
+                    Assembly assembly = Assembly.Load(ms.ToArray());
+                    Type type = assembly.GetType("RoslynCompileSample.Writer");
+                    object obj = Activator.CreateInstance(type);
+                    type.InvokeMember("Write",
+                        BindingFlags.Default | BindingFlags.InvokeMethod,
+                        null,
+                        obj,
+                        new object[] { "Hello World" });
+                }
+            }
+
+        }
+    }
+}
