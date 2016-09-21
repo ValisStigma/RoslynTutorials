76eb2927a463dd4557fc5bad1b972c98d98c0b65
ValisStigma <rafael_krucker@hotmail.com>
2016-09-21 09:20:31 +0200

    Added Example for In Memory Compilation

--- MemoryCompilationExample/MemoryCompilationExample/Program.cs
+++ MemoryCompilationExample/MemoryCompilationExample/Program.cs
@@ -1,15 +1,9 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Symbols;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Emit;
-using Microsoft.CodeAnalysis.Text;
 
 namespace MemoryCompilationExample
 {
@@ -41,32 +35,32 @@ namespace MemoryCompilationExample
 
             var compilation = CSharpCompilation.Create(
                 assemblyName,
-                syntaxTrees: new [] {syntaxTree},
-                references: references,
-                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+                new [] {syntaxTree},
+                references,
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
             );
 
             using (var ms = new MemoryStream())
             {
-                EmitResult result = compilation.Emit(ms);
+                var result = compilation.Emit(ms);
 
                 if (!result.Success)
                 {
-                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                    var failures = result.Diagnostics.Where(diagnostic =>
                         diagnostic.IsWarningAsError ||
                         diagnostic.Severity == DiagnosticSeverity.Error);
 
-                    foreach (Diagnostic diagnostic in failures)
+                    foreach (var diagnostic in failures)
                     {
-                        Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                        Console.Error.WriteLine($"{diagnostic.Id}: {diagnostic.GetMessage()}");
                     }
                 }
                 else
                 {
                     ms.Seek(0, SeekOrigin.Begin);
-                    Assembly assembly = Assembly.Load(ms.ToArray());
-                    Type type = assembly.GetType("RoslynCompileSample.Writer");
-                    object obj = Activator.CreateInstance(type);
+                    var assembly = Assembly.Load(ms.ToArray());
+                    var type = assembly.GetType("RoslynCompileSample.Writer");
+                    var obj = Activator.CreateInstance(type);
                     type.InvokeMember("Write",
                         BindingFlags.Default | BindingFlags.InvokeMethod,
                         null,
